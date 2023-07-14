d191789584e46b039abae8e14690cdceae4df770
KARUNA <KARUNA@192.168.98.205>
2023-07-14 20:12:04 +0530

    commit

---
+++ Project_ServiceManagement/src/main/java/com/qa/utilities/RetryUtils.java
@@ -0,0 +1,21 @@
+package com.qa.utilities;
+
+	import org.testng.IRetryAnalyzer;
+	import org.testng.ITestResult;
+
+	public class RetryUtils implements IRetryAnalyzer {
+		
+		int intialcount=0;
+		int maxRetryCount=3;
+		
+		@Override
+		public boolean retry(ITestResult result) {
+			if(intialcount<maxRetryCount) {
+				intialcount ++;
+				return true;
+			}
+			return false;
+		}
+		
+
+}
